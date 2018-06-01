c3b7b48ea793f7dceae60347b15b0353329004be
Unknown <torti-esser@web.de>
2018-06-01 23:00:15 +0200

    Initial Work

---
+++ js/markers.js
@@ -0,0 +1,11 @@
+// TEXT MARKERS
+var textMarkers = 
+[{
+    "name": "<span data-i18n='HIERACON'>HIERACON</span>",
+    "coords": [2964,2423]
+}, {
+    "name": "<span data-i18n='SPEZIA'>SPEZIA</span>",
+    "coords": [2045,2142]
+}]
+//Game Markers
+var markers = []
